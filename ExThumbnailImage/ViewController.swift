ac967edc4c524e3147d7b88c4d0398ddf470c14c
jake.k <jake.k@hpcnt.com>
2022-05-13 15:42:28 +0900

    Initial Commit

---
+++ ExThumbnailImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExThumbnailImage
+//
+//  Created by Jake.K on 2022/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
